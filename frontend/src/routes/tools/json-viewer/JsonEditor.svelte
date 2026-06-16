c698731aa28e1b2b6bbb55d8fa7e67de220ddd81
4BMtolobPL <108853516+4BMtolobPL@users.noreply.github.com>
2026-06-16 21:46:22 +0900

    fix: remove single quote escaping in JsonEditor

--- frontend/src/routes/tools/json-viewer/JsonEditor.svelte
+++ frontend/src/routes/tools/json-viewer/JsonEditor.svelte
@@ -37,8 +37,7 @@
 			.replace(/&/g, '&amp;')
 			.replace(/</g, '&lt;')
 			.replace(/>/g, '&gt;')
-			.replace(/"/g, '&quot;')
-			.replace(/'/g, '&#039;');
+			.replace(/"/g, '&quot;');
 
 		// Regex-based JSON highlighting
 		return escaped.replace(
